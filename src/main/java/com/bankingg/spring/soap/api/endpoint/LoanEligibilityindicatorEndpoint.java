0d65c9e53c6be68e6929228fd8b88d19048592fe
nmeka <nmeka@deloitte.com>
2020-02-11 10:29:48 +0530

    Spring-boot-soap-api-tested

---
+++ src/main/java/com/bankingg/spring/soap/api/endpoint/LoanEligibilityindicatorEndpoint.java
@@ -0,0 +1,26 @@
+package com.bankingg.spring.soap.api.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.bankingg.spring.soap.api.loaneligibility.Acknowledgement;
+import com.bankingg.spring.soap.api.loaneligibility.CustomerRequest;
+import com.bankingg.spring.soap.api.service.LoanEligibilityService;
+
+@Endpoint
+public class LoanEligibilityindicatorEndpoint {
+
+	private static final String NAMESPACE = "http://www.bankingg.com/spring/soap/api/loanEligibility";
+	@Autowired
+	private LoanEligibilityService service;
+
+	@PayloadRoot(namespace = NAMESPACE, localPart = "CustomerRequest")
+	@ResponsePayload
+	public Acknowledgement getLoanStatus(@RequestPayload CustomerRequest request) {
+		return service.checkLoanEligibility(request);
+	}
+
+}
---
+++ src/test/java/com/bankingg/spring/soap/api/SpringBootSoapWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingg.spring.soap.api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
